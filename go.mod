8a503f2ab631e8c96ab6fa5fd05f059715e896d9
jdlfounder <jdlfounder@sohu.com>
2019-10-08 10:02:04 +0800

    20191008 first add

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jdlfounder/demo
+
+go 1.13
+
+require github.com/jdlfounder/foo v1.0.3
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jdlfounder/foo"
+)
+
+func main() {
+	fmt.Println(foo.Greet("GoModule"))
+}
